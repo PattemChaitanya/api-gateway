1523fd0b71d85cdaef602ca8f5ab0a3fe7c88fff
Pattem-Chaitanya <chaitanya.pattem@ftlabs.io>
2024-05-19 17:00:12 +0530

    added redis cache

---
+++ app/utils/configs/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_URL:
+    "mongodb+srv://pattemchaitu:oBSXArjaxWwG5x5j@chaitanya.zvljb49.mongodb.net/",
+  DB_NAME: "GATEWAY_PRATICE",
+  SECRET_KEY: "this system is fucked up!@#$%&*())(*&^%$#@!",
+};
